260c51091a1e2c9e6ab3d97013822d733cf68460
Eastonboy99 <Eastonboy99@users.noreply.github.com>
2017-11-21 15:46:11 -0500

    Working Schedule Generation!!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,13 @@ import { ReactiveFormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { ClassFormComponent } from './class-form/class-form.component';
 import { SchedulesDisplayComponent } from './schedules-display/schedules-display.component';
-import { CalendarComponent } from "ap-angular2-fullcalendar";
+// import { CalendarComponent } from "ap-angular2-fullcalendar";
 
 @NgModule({
   declarations: [
     AppComponent,
     ClassFormComponent,
     SchedulesDisplayComponent,
-    CalendarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/schedules-display/schedules-display.component.ts
+++ src/app/schedules-display/schedules-display.component.ts
@@ -1,27 +1,45 @@
-import { Component, OnInit, Input } from '@angular/core';
-import { ScheduleModule } from 'primeng/primeng';
+import { Component, OnInit, Input, Output, EventEmitter, ViewChild, ElementRef, AfterViewInit } from '@angular/core';
+// import { CalendarComponent } from 'ap-angular2-fullcalendar'
+import * as $ from 'jquery';
+import 'fullcalendar';
+import {Options} from "fullcalendar";
 
 @Component({
   selector: 'app-schedules-display',
-  templateUrl: './schedules-display.component.html',
-  styleUrls: ['./schedules-display.component.css']
+  // templateUrl: './schedules-display.component.html',
+  styleUrls: ['./schedules-display.component.css'],
+  template: '<div></div>',
 })
-export class SchedulesDisplayComponent {
-  
-  @Input() calendarOptions: Object 
-  // = {
-  //   height: 650,
-  //   fixedWeekCount: false,
-  //   editable: true,
-  //   eventLimit: true, // allow "more" link when too many events
-  //   defaultView: 'agendaWeek',
-  //   minTime: "07:00:00",
-  //   maxTime: "23:00:00",
-  //   events: JSON.stringify(this.events)
-  // };
-  
-  onCalendarInit(initialized: boolean) {
-    console.log('Calendar initialized'); 
-    console.log(this.calendarOptions)
+export class SchedulesDisplayComponent implements AfterViewInit {
+  @Input() options: Options;
+  @Output() initialized: EventEmitter<boolean> = new EventEmitter<boolean>();
+  text: string;
+
+  constructor(private element:ElementRef) {
+  }
+
+  ngAfterViewInit(){
+    setTimeout(()=>{
+      console.log("100ms after ngAfterViewInit ");
+      $(this.element.nativeElement).fullCalendar(this.options);
+      this.initialized.emit(true);
+    }, 100)
+  }
+
+
+  fullCalendar(...args: any[]): any {
+    if (!args) {
+      return;
+    }
+    switch (args.length) {
+      case 0:
+        return;
+      case 1:
+        return $(this.element.nativeElement).fullCalendar(args[0]);
+      case 2:
+        return $(this.element.nativeElement).fullCalendar(args[0], args[1]);
+      case 3:
+        return $(this.element.nativeElement).fullCalendar(args[0], args[1], args[2]);
+    }
   }
 }
